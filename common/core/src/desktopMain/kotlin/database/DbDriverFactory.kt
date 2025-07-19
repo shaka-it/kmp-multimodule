46d3157905364b36dd79673501f2d51a5acd9c49
shaka <shbazhenov@gmail.com>
2025-07-19 14:37:48 +0500

    integrate sqldelight

--- common/core/src/desktopMain/kotlin/database/DbDriverFactory.kt
+++ common/core/src/desktopMain/kotlin/database/DbDriverFactory.kt
@@ -1,11 +1,14 @@
 package database
 
+import PlatformConfiguration
 import app.cash.sqldelight.db.QueryResult
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.db.SqlSchema
+import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 
-actual class DbDriverFactory actual constructor(platformConfiguration: PlatformConfiguration) {
-    actual suspend fun provideDbDriver(schema: SqlSchema<QueryResult.AsyncValue<Unit>>): SqlDriver {
-        TODO("Not yet implemented")
-    }
+actual class DbDriverFactory actual constructor(private val platformConfiguration: PlatformConfiguration) {
+    actual suspend fun provideDbDriver(
+        schema: SqlSchema<QueryResult.AsyncValue<Unit>>,
+    ): SqlDriver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
+        .also { schema.create(it).await() }
 }
